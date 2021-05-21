6ec7fdb0e3d3dbdcc3112dfa422743f16ed28bea
Víctor Azevedo de Oliveira <69988082+VictorAzevedoOliveira@users.noreply.github.com>
2021-05-21 16:46:50 -0300

    Request login usuario

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((err, conn) => {
@@ -35,6 +36,49 @@ router.post('/cadastro', (req, res, next) => {
 
     });
 });
+
+
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        const query = `SELECT * FROM tb_pessoa WHERE email_pessoa = ?`;
+        conn.query(query,[req.body.email_pessoa],(error, results, fields) => {
+            conn.release();
+            if (error) { return res.status(500).send({ error: error }) }
+            if (results.length < 1) {
+                return res.status(401).send({ mensagem: 'Falha na autenticação' })
+            }
+            bcrypt.compare(req.body.pwd_pessoa, results[0].pwd_pessoa, (err, result) => {
+                if (err) {
+                    return res.status(401).send({ mensagem: 'Falha na autenticação' })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        id_pessoa: results[0].id_pessoa,
+                        email_pessoa: results[0].email_pessoa
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    });
+                    return res.status(200).send({
+                        mensagem: 'Autenticado com sucesso',
+                        token: token
+                    });
+                }
+                return res.status(401).send({ mensagem: 'Falha na autenticação' })
+            });
+        });
+    });
+})
+
+
+
+
+
+
+
+
 
 
 
