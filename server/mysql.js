17a4def2ba9463ffe8b7e46acadb6537886979f8
unknown <tyagosilva82@gmail.com>
2021-05-22 17:29:06 -0300

    tentativa deploy heroku

--- server/mysql.js
+++ server/mysql.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 var pool = mysql.createPool({
-    "user":process.env.mysql_User,
-    "password":process.env.mysql_password,
-    "database":  process.env.mysql_database,
-    "host":process.env.mysql_host,
-    "port":process.env.mysql_port
+    "user":process.env.MYSQL_USER,
+    "password":process.env.MYSQL_PASSWORD,
+    "database":  process.env.MYSQL_DATABASE,
+    "host":process.env.MYSQL_HOST,
+    "port":process.env.MYSQL_PORT
 
 });
 exports.pool = pool;
--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/cadastro', (req, res, next) => {
