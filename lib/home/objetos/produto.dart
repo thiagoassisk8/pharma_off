ffb3075673a090b772cbd8b3b1c3ad2491ddf526
thiagoassisk8 <tyagosilva82@gmail.com>
2021-05-23 11:22:47 -0300

    [add] FrontEnd  RestApi get produtos

--- lib/home/objetos/produto.dart
+++ lib/home/objetos/produto.dart
@@ -19,13 +19,11 @@ class Produto {
     this.imgPath,
   });
 
-
   static List<Produto> list = [
     Produto(
       estabelecimento: "Rosario",
       name: "Ivermectina",
-      desc:
-          "Descrição de cada produto irá ficar aqui",
+      desc: "Descrição de cada produto irá ficar aqui",
       price: 184,
       color: AppColors.blueColor,
       imgPath: "remedio.png",
@@ -33,8 +31,7 @@ class Produto {
     Produto(
       estabelecimento: "Rosario",
       name: "Creme",
-      desc:
-          "Descrição de cada produto irá ficar aqui",
+      desc: "Descrição de cada produto irá ficar aqui",
       price: 123,
       color: AppColors.blueColor,
       imgPath: "creme.png",
@@ -42,28 +39,38 @@ class Produto {
     Produto(
       estabelecimento: "Drogasil",
       name: "Mascaras",
-      desc:
-          "Descrição de cada produto irá ficar aqui",
+      desc: "Descrição de cada produto irá ficar aqui",
       price: 135,
       color: AppColors.blueColor,
       imgPath: "mascaras.png",
     ),
   ];
 }
+
 class FlutterIcons {
   FlutterIcons._();
 
   static const _kFontFam = 'FlutterIcons';
   static const _kFontPkg = null;
 
-  static const IconData add = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData menu = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData list = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData bag = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData compass = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData person_outline = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData converse = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData search = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData nike = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData left_open_1 = IconData(0xf007, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData add =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData menu =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData list =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bag =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData compass =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person_outline =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData converse =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData nike =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData left_open_1 =
+      IconData(0xf007, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
---
+++ lib/home/rest_api/BuscaProdutos.dart
@@ -0,0 +1,23 @@
+import "dart:async";
+import 'dart:convert';
+import 'package:pharma_off/home/objetos/produto.dart';
+import 'package:http/http.dart' as http;
+
+class APIGetProdutos {
+  Future<Produto> getAllProdutos() async {
+    String url = "localhost:3000";
+    // String url = "rest-api-pharmaoff.herokuapp.com";
+
+    final http.Response response = await http.get(Uri.http(url, "/produtos"));
+    if (response.statusCode == 200) {
+      print("[DEBUG API]: ${response.statusCode}");
+      print("[DEBUG API]: ${response.body}");
+      // return Produto.fromJson(json.decode(response.body));
+    } else if (response.statusCode == 401) {
+      print("${response.statusCode}");
+      print("${response.body}");
+      // return Produto.fromJson(json.decode(response.body));
+    }
+    return null;
+  }
+}
